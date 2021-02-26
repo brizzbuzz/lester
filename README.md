cb79ca9964b3da379e6d95f8a1632d3dce872346
Ryan Brink <ryanbrink@protonmail.com>
2021-02-26 15:49:04 -0500

    modified front page lorem ipsum

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
 # Setup
 
 1. Install [NVM](https://github.com/nvm-sh/nvm#installing-and-updating)
-2. Run `nvm install 10.13.0`
-3. Install AVN
-
-```
-nvm exec 10.13.0 npm install -g avn avn-nvm avn-n
-nvm exec 10.13.0 avn setup
-```
-4. Run `nvm install 15.10.0`
+3. Install [AVN](https://github.com/wbyoung/avn#install)
 5. Install [Tauri](https://tauri.studio/en/docs/getting-started/intro#setting-up-your-environment)
 6. Clone repo
-7. Run `yarn dev` and `yarn tauri dev` to launch the react and tauri development environments 
+7. Run `yarn dev-react` and `yarn dev-rust` to launch the React and Tauri development environments 
--- pages/index.js
+++ pages/index.js
@@ -4,47 +4,37 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Lester</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Get Started &rarr;</h3>
+            <p>Learn the ropes through guided tutorials</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Plugins &rarr;</h3>
+            <p>Discover all of the integrations that Lester supports</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Lester's features</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Customize &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Learn how you can hack and modify Lester to meet your needs
             </p>
           </a>
         </div>
@@ -52,12 +42,12 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://bit.ly/37ONpcU"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/prairie.svg" alt="Prairie Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
